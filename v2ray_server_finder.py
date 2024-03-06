f605441b115366622b5733675da52e8047e4912a
Mahdi Firouzjah <44068054+mh-firouzjah@users.noreply.github.com>
2024-03-07 02:12:53 +0330

    Update v2ray_server_finder.py
    added more sorces

--- v2ray_server_finder.py
+++ v2ray_server_finder.py
@@ -1,12 +1,32 @@
 import base64
 import urllib.request
 
-url = "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/Splitted-By-Protocol/ss.txt"
+urls = {
+    "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/Splitted-By-Protocol/ss.txt",
+    "https://raw.githubusercontent.com/youfoundamin/V2rayCollector/main/ss_iran.txt",
+    "https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/Splitted-By-Protocol/ss.txt",
+    "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/sub/sub_merge.txt",
+    "https://raw.githubusercontent.com/awesome-vpn/awesome-vpn/master/all",
+    "https://raw.githubusercontent.com/freefq/free/master/v2",
+    "https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub",
+    "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2",
+    "https://raw.githubusercontent.com/AzadNetCH/Clash/main/V2Ray.txt",
+    "https://raw.githubusercontent.com/vpei/Free-Node-Merge/main/o/node.txt",
+    "https://raw.githubusercontent.com/tbbatbb/Proxy/master/dist/v2ray.config.txt",
+    "https://raw.fastgit.org/ripaojiedian/freenode/main/sub",
+    "https://github.xiaoku666.tk/https://raw.githubusercontent.com/ripaojiedian/freenode/main/sub",
+    "https://raw.githubusercontent.com/learnhard-cn/free_proxy_ss/main/v2ray/v2raysub",
+}
 secert = str()
-with urllib.request.urlopen(url) as response:
-    for line in base64.b64decode(response.read()).decode("utf8").splitlines():
-        if "🇺🇸" in line or "🇬🇧" in line:
-            secert += f"{line}\n"
+
+for url in urls:
+    with urllib.request.urlopen(url) as response:
+        try:
+            for line in base64.b64decode(response.read()).decode("utf8").splitlines():
+                if line.startswith("ss:/") and ("🇺🇸" in line or "🇬🇧" in line):
+                    secert += f"{line}\n"
+        except Exception:
+            pass
 
 with open("./ss.txt", "wb") as fl:
     fl.write(base64.b64encode(secert.encode()))
